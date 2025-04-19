82480e85bb27199f2c27ec7afdd0f48568faf056
Yassine Mouden <jasinpr9915@gmail.com>
2025-04-19 15:16:40 +0100

    Fix configurations for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+// @ts-check
+
+// Skip env validation for deployment
+process.env.SKIP_ENV_VALIDATION = "1";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
+/** @type {import('postcss').Config} */
 export default {
   plugins: {
-    "@tailwindcss/postcss": {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
